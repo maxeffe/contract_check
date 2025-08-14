e7faf9df6ac535c89b67495ede8ba6de2c96b05b
Max Fomchenkov <max.fomchenkov@gmail.com>
2025-08-14 17:43:10 +0300

    final merg

--- app/database/config.py
+++ app/database/config.py
@@ -9,11 +9,17 @@ class Settings(BaseSettings):
     DB_USER: Optional[str] = None
     DB_PASS: Optional[str] = None
     DB_NAME: Optional[str] = None
+    SECRET_KEY: Optional[str] = None
+    RABBITMQ_HOST: Optional[str] = None
+    RABBITMQ_PORT: Optional[int] = None
+    RABBITMQ_DEFAULT_USER: Optional[str] = None
+    RABBITMQ_DEFAULT_PASS: Optional[str] = None
 
     @property
     def DATABASE_URL_asyncpg(self):
         return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
     
+    @property
     @property
     def DATABASE_URL_psycopg(self):
         return f'postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
